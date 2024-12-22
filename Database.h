092ba4b663180c97680046b8d23c76f6e819fbce
Mateusz0727 <mateusz.zieba2003@gmail.com>
2024-12-22 22:51:23 +0100

    comment about how to use the database

--- Database.h
+++ Database.h
@@ -17,5 +17,8 @@ public:
     Table<T>& getTable();
 
 };
-
+//Aby miec dostêp do danych z plików trzeba dodaæ analogiczny obiekt do Table<Customer> _customer np. Table<Paczka> _paczka
+//W pliku Table.cpp Tak¿e dodaæ analogiczny template do tego który istnieje ( na górze pliku)
+//Aby program odczytywa³ poprawnie wyniki trzeba do ka¿dej klasy dodaæ przeciazenia operatorów << oraz >> analogiczne do tych które s¹ w Customer
+//jak nie dzia³a (nie tworzy siê plik txt lub nie odczytuje danych) konaktowaæ sie ze mn¹ ;)
 #endif 
